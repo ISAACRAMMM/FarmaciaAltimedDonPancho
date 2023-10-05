dd5b5e84113e4440896d0236adaa8c8ab337f044
ISAACRAMMM <isaac-ram-lop@hotmail.com>
2023-10-04 18:21:12 -0600

    Conexion a db
    conexion para mostrar productos

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,58 @@
+import express from "express";
 import { Router } from 'express';
+import pkg from "pg";
+
 
 const router= Router()
 
-router.get('/', (req, res) => res.render('index'));
+//import pool from "../database/conexion.js";
+
+
+const { Pool } = pkg;
+
+const configDB = {
+  user: "postgres",
+  host: "localhost",
+  password: "1234",
+  database: "Farmacia_altimed",
+  port: "5432",
+};
+
+const pool = new Pool(configDB);
+
+
+//router.get('/', (req, res) => res.render('index'));
 
 router.get('/contacto', (req, res) => res.render('contacto'));
 
 router.get('/ProductoDetalle', (req, res) => res.render('ProductoDetalle'));
+
+
+
+const getCategorias= async()=>{
+    try {
+        const resultado = await pool.query("SELECT * FROM \"Categorias\";"); 
+        const filas = resultado.rows;
+         
+        console.log(filas);
+        
+        return filas;
+    } catch (error) {
+        console.log(error);
+    } 
+}
+//console.log(getCategorias);
+router.get('/',async (req, res) => {
+    try {
+      const resultado= await getCategorias();
+        console.log(resultado);
+        res.render('index', { resultado: resultado});
+
+    } catch (error) {
+        console.error('Error al obtener datos de la base de datos:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
 
 export default router
